5ff7d39809493276b6fa8d89a543528df4eaebc1
redy-commits <santoschavezn@gmail.com>
2021-05-03 14:47:26 -0600

    Implementé el inicio de sesión con Google Mail, y es posible cerrar sesión tanto en Facebook como en Google Mail.

--- app/src/main/java/com/fitness_g16/Login.java
+++ app/src/main/java/com/fitness_g16/Login.java
@@ -4,53 +4,127 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //Autor/es de la clase: CN18006;
 /*
  *Esta clase sirve hacer el inicio de sesión de Facebook, vía correo electrónico y vía cuenta de Google.
  * */
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    //Constantes globales.
+    private final static int CODE_GOOGLE=777;
 
     //Variables globales que sirven para manejar el inicio de sesión de Facebook.
     private LoginButton loginFacebook;
-    private CallbackManager callbackManager=CallbackManager.Factory.create();;
-    private View view;
+    private CallbackManager callbackManager=CallbackManager.Factory.create();
 
-    //Método de Facebook ajustado para este caso particular.
+    //Variable globales que sirven para manejar el inicio de sesión de Google Mail.
+    private GoogleApiClient googleApiClient;
+    private SignInButton loginGoogle;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    //Este método se ejecuta cuando se lanza la actividad.
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);}
+    protected void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);}
+
+        //Este método se ejecuta justo después de que se finalice la actividad.
+        @Override
+        protected void onStop() {
+            super.onStop();
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);}
 
         //Método que genera la pantalla y provee la mayor parte del comportamiento de la misma.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_login);
-        loginFacebook = (LoginButton)findViewById(R.id.login_button);
+
+        //Declaración e inicialización de variables.
+        //Para cerrar la sesión de Google Mail.
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null){irAMenu();}}};
+
+        //Para iniciar sesión con Google Mail.
+        loginGoogle=(SignInButton)findViewById(R.id.loginGoogle);
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+        googleApiClient=new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+        //Para iniciar sesión con Facebook
+        loginFacebook = (LoginButton)findViewById(R.id.loginFacebook);
         loginFacebook.setReadPermissions("email");
         loginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
-            //En caso de un inicio de sesión correcto.
+            //En caso de un inicio de sesión correcto con Facebook.
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(view.getContext(), "Has iniciado sesión correctamente", Toast.LENGTH_SHORT).show();
+                irAMenu();
                 return;}
 
                 //En caso de que vuelva al menú de inicio de sesión.
             @Override
             public void onCancel() {
-                Toast.makeText(view.getContext(), "Operación cancelada", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Operación cancelada", Toast.LENGTH_SHORT).show();
                 return;}
 
                 //En caso de que suceda un error inesperado.
             @Override
             public void onError(FacebookException error) {
-                Toast.makeText(view.getContext(), "", Toast.LENGTH_SHORT).show();
-                return;}});}}
+                Toast.makeText(getApplicationContext(), "No ha sido posible iniciar sesión, inténtalo de nuevo", Toast.LENGTH_SHORT).show();
+                return;}});
+
+        //En caso de un inicio de sesión correcto con Google Mail.
+        loginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,CODE_GOOGLE);}});}
+
+                //Este método se ejecuta cuando ocurre un error durante el inicio de sesión con Google Mail.
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), "HA OCURRIDO UN ERROR", Toast.LENGTH_SHORT).show();}
+
+    //Método para comprobar si se ha iniciado sesión en Google Mail o no.
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            Toast.makeText(getApplicationContext(), "Has iniciado sesión correctamente", Toast.LENGTH_SHORT).show();
+            irAMenu();}
+        else{Toast.makeText(getApplicationContext(), "No ha sido posible iniciar sesión, inténtalo de nuevo", Toast.LENGTH_SHORT).show();}}
+
+        //Este método sirve para mandar al usuario a la actividad principal de la aplicación luego de haber iniciado sesión.
+    private void irAMenu() {
+        Intent intent = new Intent(Login.this,MainActivity.class);
+        finish();
+        startActivity(intent);}
+
+    //Método para validar el inicio de sesión.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode==CODE_GOOGLE){
+                GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                handleSignInResult(result);}else{
+                callbackManager.onActivityResult(requestCode,resultCode,data);}}}
--- app/src/main/java/com/fitness_g16/MainActivity.java
+++ app/src/main/java/com/fitness_g16/MainActivity.java
@@ -2,13 +2,24 @@ package com.fitness_g16;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Toast;
 import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,18 +32,35 @@ import androidx.appcompat.widget.Toolbar;
 /*
  *Esta clase sirve para dirigir el funcionamiento íntegro de la aplicación.
  * */
-
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     //Autor del comentario: CN18006;
-    //Única variable global hasta el momento, sospecho que sirve para controlar el menú de navegación. Esta variable se creó junto con el
-    //proyecto de manera predeterminada.
+    //Sospecho que esta variable sirve para controlar el menú de navegación. Esta variable se creó junto con el proyecto de manera predeterminada.
     private AppBarConfiguration mAppBarConfiguration;
+
+    //Variables globales para manejar la sesión de Google Mail.
+    private GoogleApiClient googleApiClient;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    //Autor del comentario: CN18006;
+    //Este método se ejecuta cuando se lanza la actividad.
+    @Override
+    protected void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);}
+
+    //Autor del comentario: CN18006;
+    //Este método se ejecuta justo después de que se finalice la actividad.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(firebaseAuthListener!=null){
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener);}}
 
     //Autor del comentario: CN18006;
     /*
-    * Método principal de la clase, acá se encuentra el código que permite ejecutar y controlar el resto de las actividades de la
-    * aplicación.
+    * Método principal de la clase, acá se encuentra el código que permite ejecutar y controlar el resto de las actividades de la aplicación.
     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,19 +69,40 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         //Autor del comentario: CN18006;
+        //Declaración de variables relacionadas con el LogOut de Google Mail.
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //Obtener los datos del usuario.
+                }}};
+
+        //Autor del comentario: CN18006;
+        //Declaración de variables relacionadas con el LogIn silencioso de Google Mail.
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient=new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+        //Autor del comentario: CN18006;
         /*
          * Esta sección del código sirve para controlar que el usuario de la aplicación tenga una sesión activa antes de utilizar todas las
          * funciones de la aplicación.
          * */
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
-        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        boolean loggedFacebook = accessToken != null && !accessToken.isExpired();
 
-        if(isLoggedIn){
+        if(loggedFacebook){
             //Si queda tiempo, agregar un listado de frases para que no aparezca el mismo texto siempre (requerimiento no funcional).
             Toast.makeText(this,"Bienvenido/a",Toast.LENGTH_SHORT).show();}
         else{
-            irALogin();
-            Toast.makeText(this,"Primero tienes que iniciar sesión",Toast.LENGTH_SHORT).show();}
+            OptionalPendingResult<GoogleSignInResult> opr=Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+            if(opr.isDone()){
+                if(opr.get().isSuccess()){
+                    Toast.makeText(this,"Bienvenido/a",Toast.LENGTH_SHORT).show();}}
+            else{
+                irALogin();
+                Toast.makeText(this,"Primero tienes que iniciar sesión",Toast.LENGTH_SHORT).show();}}
         //Fin de la sección señalada.
 
         //Autor del comentario: CN18006;
@@ -80,18 +129,57 @@ public class MainActivity extends AppCompatActivity{
         // Método que envía al usuario a la pantalla de inicio de sesión si aún no ha iniciado sesión.
     private void irALogin(){
         Intent intent = new Intent(MainActivity.this,Login.class);
+        finish();
         startActivity(intent);}
 
     //Autor del comentario: CN18006;
-    //Los siguientes dos métodos fueron generados automáticamente en la creación del proyecto, y me es de poco interés conocer para qué se
-    // están usando.
+
+    //Este método sirve para mandar al usuario a la actividad principal de la aplicación, actualmente se está usando para conocer si es necesario
+    // que inicie sesión.
+    private void irAMenu(){
+        Intent intent = new Intent(MainActivity.this,MainActivity.class);
+        finish();
+        startActivity(intent);}
+
+    //Autor del comentario: CN18006;
+    //Este método sirve para controlar cuáles opciones del menú derecho debe ver el usuario según el proveedor de su cuenta.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean loggedFacebook = accessToken != null && !accessToken.isExpired();
+
+        if(loggedFacebook){getMenuInflater().inflate(R.menu.main_facebook, menu);}
+        else{
+            OptionalPendingResult<GoogleSignInResult> opr=Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+            if(opr.isDone()){
+                if(opr.get().isSuccess()){
+                    getMenuInflater().inflate(R.menu.main_google, menu);}}}
         return true;}
 
+    //Autor del comentario: CN18006;
+    //Este método sirve para cerrar la sesión activa del usuario de Facebook.
+    public void logOutFacebook(MenuItem item){
+        LoginManager.getInstance().logOut();
+        irAMenu();}
+
+    //Autor del comentario: CN18006;
+    //Este método sirve para cerrar la sesión activa del usuario de Google Mail.
+    public void logOutGoogle(MenuItem item) {
+        firebaseAuth.signOut();
+        Auth.GoogleSignInApi.signOut(googleApiClient);
+        irAMenu();}
+
+    //Autor del comentario: CN18006;
+    //Este método se generó de forma automática en la creación del proyecto y me es de poco interés conocer para qué se está usando.
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();}}
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();}
+
+    //Autor del comentario: CN18006;
+    //Este método se ejecuta cuando ocurre un error durante el inicio de sesión con Google Mail.
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "HA OCURRIDO UN ERROR", Toast.LENGTH_SHORT).show();}}
